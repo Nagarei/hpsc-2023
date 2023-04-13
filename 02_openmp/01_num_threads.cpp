2aca011a1de093c338371c95b2c9cf5265c0b44f
Reiji Nagata <Nagarei@users.noreply.github.com>
2023-04-13 09:58:47 +0900

    test omp_set_num_threads

--- 02_openmp/01_num_threads.cpp
+++ 02_openmp/01_num_threads.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <omp.h>
 
+void func() {
+   #pragma omp parallel
+  std::cout << "func hello\n";
+}
+
 int main() {
+  func();
+
   omp_set_num_threads(3);
-#pragma omp parallel num_threads(2)
+#pragma omp parallel
+//num_threads(2)
   std::cout << "hello\n";
+
+  func();
 }
