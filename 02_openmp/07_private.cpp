4d0f688eb1574593fc5da43df4139e316664212e
Reiji Nagata <Nagarei@users.noreply.github.com>
2023-04-17 09:29:57 +0900

    arr private

--- 02_openmp/07_private.cpp
+++ 02_openmp/07_private.cpp
@@ -1,24 +1,26 @@
 #include <cstdio>
 
 int main() {
-  int a = 1, b = 1, c = 1;
+  int tmp[4] = {1,1,1,1};
+  int a[1] = {1}, *b = tmp, c[1] = {-1};
+  
 #pragma omp parallel num_threads(4)
   {
 #pragma omp for private(a)
     for(int i=0; i<4; i++)
-      printf("%d ",++a);
+      printf("%d ",++a[0]);
 #pragma omp single
     printf("\n");
 #pragma omp for firstprivate(b)
     for(int i=0; i<4; i++)
-      printf("%d ",++b);
+      printf("%d ",++*b);
 #pragma omp single
     printf("\n");
 #pragma omp for lastprivate(c)
     for(int i=0; i<4; i++)
-      printf("%d ",++c);
+      printf("%d ",++*c);
 #pragma omp single
     printf("\n");
   }
-  printf("%d %d %d\n",a,b,c);
+  printf("%d %d %d\n",*a,*b,*c);
 }
