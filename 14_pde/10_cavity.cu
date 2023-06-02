30b8e638fec1137175a05bb17a7d470aa40ebd43
Reiji Nagata <Nagarei@users.noreply.github.com>
2023-06-02 21:15:13 +0900

    cuda 10_cavity.cu

--- 14_pde/10_cavity.cu
+++ 14_pde/10_cavity.cu
@@ -20,7 +20,7 @@ constexpr double rho = 1;
 constexpr double nu = 0.02;
 
 template<typename T>
-inline constexpr T pow2(const T& v) {
+__host__ __device__ inline constexpr T pow2(const T& v) {
 	return v * v;
 }
 
@@ -77,34 +77,34 @@ del p
 )";
 }
 
-void step1(double* out_b, double* prev_u, double* prev_v, int idx) {
-	//const int idx = blockIdx.x * blockDim_x + threadIdx.x;
+__global__ void step1(double* out_b, double* prev_u, double* prev_v) {
+	const int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	const auto un = reinterpret_cast<const double(*)[nx]>(prev_u);
 	const auto vn = reinterpret_cast<const double(*)[nx]>(prev_v);
 	const auto b = reinterpret_cast<double(*)[nx]>(out_b);
 	const int h = 1 + (idx / (nx - 2));
 	const int w = 1 + (idx % (nx - 2));
 
+	if (ny - 1 <= h) { return; }
 	assert(1 <= h && h < ny - 1);
 	assert(1 <= w && w < nx - 1);
-	if (ny - 1 <= h) { return; }
 	b[h][w] = rho * (1 / dt * \
 		((un[h][w + 1] - un[h][w - 1]) / (2 * dx) + (vn[h + 1][w] - vn[h - 1][w]) / (2 * dy)) -
 		pow2((un[h][w + 1] - un[h][w - 1]) / (2 * dx)) - 2 * ((un[h + 1][w] - un[h - 1][w]) / (2 * dy) *
 			(vn[h][w + 1] - vn[h][w - 1]) / (2 * dx)) - pow2((vn[h + 1][w] - vn[h - 1][w]) / (2 * dy)));
 }
 
-void step2(double* out_p, double* prev_p, double* now_b, int idx) {
-	//const int idx = blockIdx.x * blockDim_x + threadIdx.x;
+__global__ void step2(double* out_p, double* prev_p, double* now_b) {
+	const int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	const auto p = reinterpret_cast<double(*)[nx]>(out_p);
 	const auto pn = reinterpret_cast<const double(*)[nx]>(prev_p);
 	const auto b = reinterpret_cast<const double(*)[nx]>(now_b);
 	int h = (idx / nx);
 	int w = (idx % nx);
 
+	if (ny <= h) { return; }
 	assert(0 <= h && h < ny);
 	assert(0 <= w && w < nx);
-	if (ny <= h) { return; }
 	auto& out = p[h][w];
 	if (w == nx - 1) { w = nx - 2; }
 	else if (w == 0) { w = 1; }
@@ -124,8 +124,8 @@ void step2(double* out_p, double* prev_p, double* now_b, int idx) {
 	}
 }
 
-void step3(double* out_u, double* out_v, double* prev_u, double* prev_v, double* now_p, int idx) {
-	//const int idx = blockIdx.x * blockDim_x + threadIdx.x;
+__global__ void step3(double* out_u, double* out_v, double* prev_u, double* prev_v, double* now_p) {
+	const int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	const auto u = reinterpret_cast<double(*)[nx]>(out_u);
 	const auto v = reinterpret_cast<double(*)[nx]>(out_v);
 	const auto un = reinterpret_cast<const double(*)[nx]>(prev_u);
@@ -134,9 +134,9 @@ void step3(double* out_u, double* out_v, double* prev_u, double* prev_v, double*
 	int h = (idx / nx);
 	int w = (idx % nx);
 
+	if (ny <= h) { return; }
 	assert(0 <= h && h < ny);
 	assert(0 <= w && w < nx);
-	if (ny <= h) { return; }
 	if (h == ny - 1) {
 		u[h][w] = 1;
 		v[h][w] = 0;
@@ -173,40 +173,39 @@ void run()
 	//static std::array<double, ny> y;
 	//for (int i = 0; i < ny; ++i) { y[i] = dy * i; }
 
-	static std::array<std::array<double, nx>, ny> u_[2], v_[2], p_[2], b;
+	static std::array<std::array<double, nx>, ny> host_u, host_v, host_p;
+	double* u_;
+	double* v_;
+	double* p_;
+	double* b;
+	cudaMalloc((void**)&u_, 2*nx*ny*sizeof(double)); cudaMemsetAsync(u_, 0, 2*nx*ny*sizeof(double));
+	cudaMalloc((void**)&v_, 2*nx*ny*sizeof(double)); cudaMemsetAsync(v_, 0, 2*nx*ny*sizeof(double));
+	cudaMalloc((void**)&p_, 2*nx*ny*sizeof(double)); cudaMemsetAsync(p_, 0, 2*nx*ny*sizeof(double));
+	cudaMalloc((void**)&b, nx*ny*sizeof(double));
 	for (int n = 0; n < nt; ++n) {
 		//un = u.copy();
 		//vn = v.copy();
-		auto& u = u_[n % 2];
-		auto& un = u_[(n % 2) ^ 1];
-		auto& v = v_[n % 2];
-		auto& vn = v_[(n % 2) ^ 1];
+		auto u = u_ + ((n % 2 == 0) ? 0 : nx * ny);
+		auto un = u_ + ((n % 2 != 0) ? 0 : nx * ny);
+		auto v = v_ + ((n % 2 == 0) ? 0 : nx * ny);
+		auto vn = v_ + ((n % 2 != 0) ? 0 : nx * ny);
 
-		for (int h = 1; h < ny - 1; ++h) {
-			for (int w = 1; w < nx - 1; ++w) {
-				step1(b.data()->data(), un.data()->data(), vn.data()->data(), (h - 1) * (nx - 2) + (w - 1));
-			}
-		}
-		//
+		constexpr int block_num = (nx*ny + 1023) / 1024;
+		step1<<<block_num, 1024>>>(b,un,vn);
 		for (int it = 0; it < nit; ++it) {
-			auto& p = p_[(n + it) % 2];
-			auto& pn = p_[((n + it) % 2) ^ 1];
-			//pn = p.copy();
-			for (int h = 0; h < ny; ++h) {
-				for (int w = 0; w < nx; ++w) {
-					step2(p.data()->data(), pn.data()->data(), b.data()->data(), h * nx + w);
-				}
-			}
+			auto p = p_ + (((n + it) % 2 == 0) ? 0 : nx * ny);
+			auto pn = p_ + (((n + it) % 2 != 0) ? 0 : nx * ny);
+			step2<<<block_num, 1024>>>(p, pn, b);
 		}
-		auto& p = p_[((n + nit) % 2) ^ 1];
-		for (int h = 0; h < ny; ++h) {
-			for (int w = 0; w < nx; ++w) {
-				step3(u.data()->data(), v.data()->data(), un.data()->data(), vn.data()->data(), p.data()->data(), h * nx + w);
-			}
-		}
+		auto p = p_ + (((n + nit) % 2 != 0) ? 0 : nx * ny);
+		step3<<<block_num, 1024>>>(u, v, un, vn, p);
 
+		cudaMemcpyAsync(host_u[0].data(), u, nx*ny*sizeof(double), cudaMemcpyDeviceToHost);
+		cudaMemcpyAsync(host_v[0].data(), v, nx*ny*sizeof(double), cudaMemcpyDeviceToHost);
+		cudaMemcpyAsync(host_p[0].data(), p, nx*ny*sizeof(double), cudaMemcpyDeviceToHost);
 		pyplot_out << "plt.title(\"" << n << "\")\n";
-		pyplot(u, v, p);
+		cudaDeviceSynchronize();
+		pyplot(host_u, host_v, host_p);
 	}
 
 	//finalize pyplot
